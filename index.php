4cff84d88e878fd71db5f86e2486f3bc481e1f98
AbdFaris513 <106486197+AbdFaris513@users.noreply.github.com>
2022-08-16 20:10:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mobile Shop</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <header>
+            <h1>
+                MM Mobiles
+            </h1>
+            <div class="mid">
+                <!--  -->
+            </div>
+            <div class="menu">
+                <h3>
+                    <a href="">Location</a>
+                </h3>
+                <h3>
+                    <a href="">Contact</a>
+                </h3>
+            </div>
+        </header>
+    </div>
+    <div class="content">
+        <h2 class="brand">Top Brands</h2>
+        <div class="image">
+            <div class="i">
+                <a href="apple.html">
+                    <img src="image/apple.jpg" alt="Iphone" width="200px" height="200px">
+                </a>
+                <h3>IPhone</h3>
+            </div>
+            <div class="i">
+                <a href="">
+                    <img src="image/google.jpg" alt="Samsung" width="200px">
+                </a>
+                <h3>Google Pixel</h3>
+            </div>
+            <div class="i">
+                <a href="">
+                    <img src="image/samsung.jpg" alt="Samsung" width="200px"height="200px" >
+                </a>
+                <h3>Samsang</h3>
+            </div>
+            <div class="i">
+                <a href="">
+                    <img src="image/realme.jpg" alt="realme" width="200px">
+                </a>
+                <h3>Realme</h3>
+            </div>
+            <div class="i">
+                <a href="">
+                    <img src="image/redme.jpg" alt="Samsung" width="200px">
+                </a>
+                <h3>Redme</h3>
+            </div>
+            <div class="i">
+                <a href="">
+                    <img src="image/1+p.jpg" alt="realme" width="200px">
+                </a>
+                <h3>One Pluse</h3>
+            </div>
+            <div class="i">
+                <a href="">
+                    <img src="image/Oppo-A77-Sky-Blue-1.jpg" alt="Samsung" width="200px">
+                </a>
+                <h3>Oppo</h3>
+            </div>
+            <div class="i">
+                <a href="">
+                    <img src="image/vivo.jpg" alt="realme" width="200px">
+                </a>
+                <h3>Vivo</h3>
+            </div>
+            <div class="i">
+                <a href=""><img src="image/huwai.jpg" alt="realme" width="200px"></a>
+                <h3>Huwai</h3>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
